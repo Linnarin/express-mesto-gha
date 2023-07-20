ed776cec7c75d9f3866accbfdd7a3ac53df2016b
Alina <maximovaalina5@gmail.com>
2023-07-20 22:38:12 +0300

    14 спринт

--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,14 @@ const express = require('express');
 const router = express.Router();
 
 const {
-  getUsers, getUser, updateUser, updateUsersAvatar,
+  getUsers, getCurrentUser, updateUser, updateUsersAvatar,
 } = require('../controllers/users');
 
 const celebrate = require('../middlewares/celebrate');
 
 router.get('/', getUsers);
-router.get('/me', getUser);
-router.get('/:id', celebrate.validateUserId, getUser);
+router.get('/me', getCurrentUser);
+router.get('/:id', celebrate.validateUserId, getCurrentUser);
 router.patch('/me', celebrate.validateUpdateUser, updateUser);
 router.patch('/me/avatar', celebrate.validateUserAvatar, updateUsersAvatar);
 
